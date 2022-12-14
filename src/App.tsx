2b6f6d4375f60192d7d0d9fe58044274664f64be
Edwin Dijas Chiwona <c-edwin.chiwona@on24.com>
2022-12-14 18:38:11 +0200

    PT-4 Fix global styling CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import GlobalCss from 'styles/global'
 
 function App() {
   return (
     <div className="App">
+      <GlobalCss />
       Personallity Test
     </div>
   );
