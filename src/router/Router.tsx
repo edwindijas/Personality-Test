da1da488617cc7c18fd441f4a5ad26694bdef642
Edwin Dijas Chiwona <c-edwin.chiwona@on24.com>
2022-12-15 20:41:44 +0200

    PT-18 Add Style to Router to center inner components

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { HomePage } from "pages/home/HomePage";
 import { ResultPage } from "pages/result/ResultPage";
 import { TestPage } from "pages/test/TestPage";
 import { Page } from "pages/types";
+import { PageEle } from "./Router.style";
 
 const Pages: { [key in Page]: () => JSX.Element } = {
     home: HomePage,
@@ -14,7 +15,7 @@ const Pages: { [key in Page]: () => JSX.Element } = {
 export const Router = () => {
     const { state } = useAppState();
     const Page = Pages[state.page];
-    return <div >
+    return <PageEle >
         <Page />
-    </div>
+    </PageEle>
 }
