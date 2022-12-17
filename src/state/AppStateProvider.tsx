d6d8a6311a5a5deea1390bc731d4d7352b9e950a
Edwin Dijas Chiwona <c-edwin.chiwona@on24.com>
2022-12-17 21:21:42 +0200

    PT-25 Implement initial questions fetch

--- src/state/AppStateProvider.tsx
+++ src/state/AppStateProvider.tsx
@@ -1,7 +1,8 @@
-import { createContext, useReducer } from "react"
+import { createContext, useEffect, useReducer } from "react"
 import { AppState, AppStateProviderProps, AppStateContextValue } from "./types"
 import { appStateReducer } from "reducers"
 import { english } from "i18n/default.local";
+import { Questions } from "models/Question";
 
 export const AppStateContext = createContext<AppStateContextValue>({} as AppStateContextValue);
 
@@ -16,7 +17,14 @@ const defaultAppState: AppState = {
 export const AppStateProvider = ( {children}: AppStateProviderProps ) => {
 
     const [state, dispatch] = useReducer(appStateReducer, defaultAppState)
+    useEffect(() => {
 
+        Questions.get().then(data => {
+            const questions = Questions.addId(data)
+            dispatch({ store: 'question',type: 'addAll', payload: { questions } })
+        })
+
+    }, [])
     return <AppStateContext.Provider value={{state, dispatch}} >
         { children }
     </AppStateContext.Provider>
