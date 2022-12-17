8c93c7af906158989c8ee0cede2b406aeb67f61b
Edwin Dijas Chiwona <c-edwin.chiwona@on24.com>
2022-12-17 21:08:47 +0200

    PT-21 Fix overflow router shell

--- src/router/Router.style.tsx
+++ src/router/Router.style.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const PageEle = styled.div`
-    width: 100vw;
+    width: 100%;
     min-height: 100vh;
     display: flex;
     align-items: center;
