5562f2b4333054e0f6e5676afeb2f9548a5dbb82
Edwin Dijas Chiwona <c-edwin.chiwona@on24.com>
2022-12-18 15:25:04 +0200

    PT-31 Add e2e test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, fireEvent, render, screen } from "@testing-library/react"
+import App from "App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const homePage = screen.getByTestId('home-page');
-  expect(homePage).toBeInTheDocument();
-});
+import mockFetch from 'mock/mockFetch'
+
+const questions = getMockQuestions()
+
+beforeEach(async () => {
+    jest.spyOn(window, 'fetch').mockImplementation(mockFetch)
+    await act(async () => {
+      render(<App />)
+    })
+})
+
+describe('User can move from home to results page and restart', () => {
+    it ('Home to Test Page', async () => {
+        const { getByTestId, getAllByTestId } = screen;
+        
+        expect(getByTestId('home-page')).toBeInTheDocument()
+
+        await act (() => {
+            const { getByTestId } = screen;
+            const buttonNext = getByTestId('home-page-btn-next');
+            fireEvent.click(buttonNext);
+        })
+
+        const testPage = getByTestId('test-page');
+        expect(testPage).toBeInTheDocument();
+
+               
+        for (let x = 0; x < questions.length; x++) {
+            const answer = getAllByTestId('answer');
+            const btnNext = getByTestId('test-page-btn-next') as HTMLButtonElement;
+
+            await act(() => {
+                fireEvent.click(answer[0]);
+            })
+
+            await act(() => {
+                fireEvent.click(btnNext);
+            })
+        }
+
+        const resultsPage = getByTestId('result-page');
+        expect(resultsPage).toBeInTheDocument();
+
+       await act(() => {
+        fireEvent.click(getByTestId('result-page-btn-restart'))
+       })
+
+       expect(getByTestId('home-page')).toBeInTheDocument();
+
+    })
+})
